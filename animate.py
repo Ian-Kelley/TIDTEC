19c92a045b4291cfd97ab25e20b9f69e7b69d750
Ian-Kelley <ikelley@vt.edu>
2021-10-27 21:00:13 -0400

    conversion and animation

--- animate.py
+++ animate.py
@@ -1,22 +1,12 @@
 import matplotlib.pyplot as plt
 import datetime as dt
 import cartopy.crs as ccrs
-import h5py
 import pandas as pd
-import numpy as np
 from cartopy.feature.nightshade import Nightshade
-from IPython.display import Video
-import matplotlib.animation as animation
-from cartopy.io.img_tiles import OSM
-import cartopy.io.img_tiles as cimgt
-from matplotlib.tri import Triangulation, UniformTriRefiner
-import os
 
 
-
-
-def plot_still(time, df, i, frame_code):
-    #central = 180 - (time.hour * 15 + time.minute * .25 + time.second * .0041667)
+def plot_still(time, df, frame_code):
+    plt.close()
     tecmap = plt.figure(figsize=(16, 10))
     ax = plt.axes(projection = ccrs.PlateCarree())
     ax.coastlines(color='black', zorder=1)
@@ -25,32 +15,31 @@ def plot_still(time, df, i, frame_code):
     ax.add_feature(Nightshade(time, alpha=0.15))
     ax.set_title(str(time), size=30)
     part = df[df['datetime'] == time]
-    if(len(part.tec)) < 25000:#reurn if not enough points
+    if(len(part.tec)) < 1000:#reurn if not enough points
         return frame_code
-    tri = Triangulation(part.glon, part.gdlat)
-    tri = UniformTriRefiner(tri)
-    levels = np.arange(0., 2., .005)
-    mesh = ax.scatter(part.glon, part.gdlat, c=part.tec, transform=ccrs.PlateCarree(), vmin=0, vmax=30, cmap='jet', s=5, alpha=1)
-    #mesh = ax.scatter(tri, part.log_tec, transform=ccrs.PlateCarree(), levels=levels, cmap='jet')#, vmin=0, vmax=25, alpha=0.5, s=100, cmap='jet')
-    
+    mesh = ax.scatter(part.glon, part.gdlat, c=part.tec, transform=ccrs.PlateCarree(), vmin=0, vmax=50, cmap='plasma', s=40, alpha=.7)
     cbar = plt.colorbar(mesh, fraction=0.046, pad=0.04)
-    cbar.set_label('log 10 TECu')
+    cbar.set_label('TECu')
     plt.savefig(f'{frame_code:03d}' + '.png')
     plt.close()
     return frame_code + 1
 
-def plot_series(df):
-	start = dt.datetime(2017, 9, 8, 0)
-	cadence = dt.timedelta(seconds = 30)
-	frame_num = 15
-	frame_attempt = 0
-	frame_code = 0
-	while frame_attempt < frame_num:
-    		time = start + frame_attempt * cadence
-    		frame_attempt = frame_attempt + 1
-    		frame_code = plot_still(time, df, frame_attempt, frame_code)
+def plot_series(df, start, end, cadence):
+    frame_num = int((end - start) / cadence)
+    frame_attempt = 0
+    frame_code = 0
+    while(frame_attempt < frame_num):
+        time = start + (frame_attempt * cadence)
+        frame_attempt = frame_attempt + 1
+        frame_code = plot_still(time, df, frame_code)
 
 if __name__ == "__main__":
-    df = pd.read_pickle('los_20170908.pkl')
+    #change these
+    start = dt.datetime(2015, 1, 7, 21)
+    end = dt.datetime(2015, 1, 7, 23)
+    cadence = dt.timedelta(seconds = 30) 
+    df = pd.read_pickle('los_20150107.pkl')
     df = df.where(df['elm'] > 30).dropna()
-    plot_series(df)
+    plot_series(df, start, end, cadence)
+
+
--- conversion.py
+++ conversion.py
@@ -1,7 +1,6 @@
 import h5py
-import numpy as np
+import datetime as dt
 import pandas as pd
-import os
 import gc
 
 
@@ -28,12 +27,49 @@ def get_len(filename):
     size = int(data.size / 2)
     return size
 
-if __name__ == '__main__':
-    filename = 'los_20140326.001.h5'
-    df = quick_convert(filename, 0, -1)
+def get_df():
+    df1 = pd.read_pickle('los_20140326.pkl')
+    #print('df1 load')
+    #df2 = pd.read_pickle('los_20170907_early.pkl')
+    #print('df2 load')
+    #df = pd.concat([df1, df2])
+    #del df1
+    #del df2
+    return df1
+
+def clean_df(df, start, end, extent):
+    df = df[df['datetime'] > start]
+    df = df[df['datetime'] < end]
+    df = df[df['elm'] > 30]
+    df = df[df['gdlat'] > extent[0]]
+    df = df[df['gdlat'] < extent[1]]
+    df = df[df['glon'] > extent[2]]
+    df = df[df['glon'] < extent[3]]
+    df['code'] = (df['gps_site'].str.decode('UTF-8') + '_' + df['sat_id'].astype(int).astype(str))
+    df = df.drop(columns = ['gps_site', 'sat_id'])
+    return df
+
+def detrend_df(df):
+    df = df.sort_values(by=['code', 'datetime'])
+    df['30min_detrend'] = df['tec'] - df.rolling(60, center=True, min_periods=30).mean().tec
+    return df
+
+
+if __name__ == "__main__":
+    #change these
+    filename = 'los_20150107.001.h5'
+    start = dt.datetime(2015, 1, 7, 21)
+    end = dt.datetime(2015, 1, 7, 23)
+    extent = [20, 70, -145, -60]
+
+    #conversion script
+    len = get_len(filename)
+    df = quick_convert(filename, int(len * 3 / 4), -1)
     print('df converted')
-    df.to_pickle(filename[0:12] + '.pkl')
-
-
+    df = clean_df(df, start, end, extent)
+    print('df cleaned')
+    #df = detrend_df(df)
+    #print('df detrended')
+    df.to_pickle(filename.partition(".")[0] + '.pkl')
 
  
